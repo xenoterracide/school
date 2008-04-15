c3d382a16aea188dca8388994b1b2705bd6e0e17
Caleb Cushing <xenoterracide@gmail.com>
2008-04-15 17:19:42 -0400

    finished assign1 problem 5

--- perl/assign1/number_5.pl
+++ perl/assign1/number_5.pl
@@ -1,7 +1,17 @@
 #!/usr/bin/perl
 @names=qw(Nick Susan Chet Dolly Bill);
-print "@names\n";
-splice(@names, 1, 2, ellie, beatrice, charles);
-print "@names\n";
-splice(@names, 5);
-print "@names\n";
+print  "@names\n";
+splice (@names, 1, 2, ellie, beatrice, charles);
+print  "@names\n";
+splice (@names, 5);
+print  "@names\n";
+push   (@names, "lewis", "izzy");
+print  "@names\n";
+shift   @names; #seems like a hack but splice didn't work
+print  "@names\n";
+@names=reverse(@names);
+print  "@names\n";
+unshift(@names, "archie");
+print  "@names\n";
+@names=sort(@names);
+print  "@names\n";
