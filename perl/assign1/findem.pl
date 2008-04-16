b6153c4b3da5c41af80f83db8192a78c5f95b3de
Caleb Cushing <xenoterracide@gmail.com>
2008-04-16 06:55:19 -0400

    expanding test case. findem works, although not perhaps as prescribed in book. I like mine better.

--- perl/assign1/findem.pl
+++ perl/assign1/findem.pl
@@ -1,11 +1,9 @@
 #!/usr/bin/perl
-use warnings;
 print "who are you searcing for? ";
 chomp($search=<STDIN>);
 print "search: $search\n";
 while(my $line = <>) {
 	($name, $phone, $salary) = split(":", $line);
-	print "search: $search\n";
 	$count   = grep( /$search/i, $name);
 	while ($count != 0) {
 		@results = grep( /$search/i, $name);
@@ -14,5 +12,6 @@ while(my $line = <>) {
 		chomp($newphone=<STDIN>);
 		$phone=$newphone;
 		print "@results new phone number is $phone\n";
+		$count = 0
 	}
 }
