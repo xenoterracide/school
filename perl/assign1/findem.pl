624692445b4a19b4b504a90a5f7a18aa176156b2
Caleb Cushing <xenoterracide@gmail.com>
2008-04-16 06:51:25 -0400

    update test data. findem.pl works except for infinite loop :(

--- perl/assign1/findem.pl
+++ perl/assign1/findem.pl
@@ -4,10 +4,15 @@ print "who are you searcing for? ";
 chomp($search=<STDIN>);
 print "search: $search\n";
 while(my $line = <>) {
-	@fields = split(":", $line);
-	chomp (@fields);
-	print "fields: @fields\n";
+	($name, $phone, $salary) = split(":", $line);
 	print "search: $search\n";
-	@results = grep( /$search/i, @fields);
-	print "results: @results\n";
+	$count   = grep( /$search/i, $name);
+	while ($count != 0) {
+		@results = grep( /$search/i, $name);
+		print "results: @results\nphone: $phone\n";
+		print "new phone number? ";
+		chomp($newphone=<STDIN>);
+		$phone=$newphone;
+		print "@results new phone number is $phone\n";
+	}
 }
