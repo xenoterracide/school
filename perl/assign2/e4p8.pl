ba9bce90e3a3feec916033f8b7030774f7d83fc0
Caleb Cushing <xenoterracide@gmail.com>
2008-04-20 22:53:25 -0400

    add array

---
+++ perl/assign2/e4p8.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+
+@saying=(
+	"an apple a day keeps the docor away",
+	"procrastination is a thief in time",
+	"the early bird catches the worm",
+	"handsome is is handsome does",
+	"too many cooks spoil the broth"
+;
