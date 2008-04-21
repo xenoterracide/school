452d6fb2833ba5972e8ec911d6052bb233940661
Caleb Cushing <xenoterracide@gmail.com>
2008-04-20 23:05:21 -0400

    update comments

--- perl/assign2/e4p8.pl
+++ perl/assign2/e4p8.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl
-
-
+# (c) caleb cushing 2008, license GPLv3
+#
+# note the proper way to write this would include a count of the elements in 
+# the array and then use that number as a variable for the rand function
 @saying=(
 	"an apple a day keeps the docor away",
 	"procrastination is a thief in time",
