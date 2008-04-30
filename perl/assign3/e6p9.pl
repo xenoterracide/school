589c7afd6af47d01b368c5ef04c534ea9c6a25c9
Caleb Cushing <xenoterracide@gmail.com>
2008-04-29 21:10:47 -0400

    perl: added newline to exercise 6 prolem 9

--- perl/assign3/e6p9.pl
+++ perl/assign3/e6p9.pl
@@ -2,5 +2,5 @@
 
 while(my $line = <>) {
 	($name, $phone, $address, $bday, $salary) = split(":", $line);
-	print $bday if $name =~ /Tommy\ Savage/;
+	print "$bday\n" if $name =~ /Tommy\ Savage/;
 }
