f239126c0df0dfa592b443875bea714662853435
Caleb Cushing <xenoterracide@gmail.com>
2008-04-30 21:30:35 -0400

    more updates from jessica

---
+++ adv_html/animal_proj/js/ccImageSwitcher.js
@@ -0,0 +1,13 @@
+var aryImages = new Array();
+ 
+aryImages[1] = "images/platypus1.jpg";
+aryImages[2] = "images/platypus2.jpg";
+ 
+for (i=0; i < aryImages.length; i++) {
+  var preload = new Image();
+  preload.src = aryImages[i];
+}
+ 
+function swap( imgIndex, imgTarget) {
+  document[imgTarget].src = aryImages[imgIndex];
+}
