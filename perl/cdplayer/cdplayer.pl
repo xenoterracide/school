f58e60de0f778776203d85af93328d7c40f38241
Caleb Cushing <xenoterracide@gmail.com>
2008-05-23 11:38:45 -0400

    built options for search

--- perl/cdplayer/cdplayer.pl
+++ perl/cdplayer/cdplayer.pl
@@ -14,18 +14,33 @@ sub prompt
 {
 	my $offset = 0;
 	do {
-		print "1:\tmodify collection\n";
-		print "4:\texit\n";
-		print "Enter an offset: ";
-		$input = <>;
-		chomp($input);
+		print "1:\tsearch\n";
+		print "9:\texit\n";
+		print "Enter an option: ";
+		chomp($input = <>);
 		if ( $input == 1) {
-			&mod_db;
+			&search;
 		}
-	}
-	while ($input != 4);
+	} while ($input != 9);
 }
 
+sub search {
+	do {
+		print "Search\n";
+		print "1:\tby Track\n";
+		print "2:\tby Artist\n";
+		print "3:\tby Album\n";
+		print "4:\tby Year\n";
+		print "8:\treturn to main\n";
+		print "9:\texit\n";
+		print "Enter an option: ";
+		chomp($input = <>);
+		chomp($input);
+		if ($input == 8) {
+			&prompt;
+		}
+	} while ($input != 9);
+}
 sub mod_db {
 	print "mod_db placeholder\n";
 }
