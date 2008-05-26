9403ce9f00ccc1490e58b599e4859ae68214882e
Caleb Cushing <xenoterracide@gmail.com>
2008-05-26 13:35:36 -0400

    add search for year and artist

--- perl/cdplayer/cdplayer.pl
+++ perl/cdplayer/cdplayer.pl
@@ -41,7 +41,7 @@ sub search {
 		print "9:\texit\n";
 		print "Enter an option: ";
 		chomp($menu_input = <>);
-		if ($menu_input == 1) {
+		if ($menu_input == 1) { # track
 			print "Track to search for: ";
 			chomp($search_input = <>);
 			print "\n"; # filler blank line
@@ -53,12 +53,36 @@ sub search {
 					print "\tYear:\t$cd_db[$idx][3]\n";
 				}
 			}
-		} elsif ($menu_input == 3) {
+		} elsif ($menu_input == 2) { # artist
+			print "Artist to search for: ";
+			chomp($search_input = <>);
+			print "\n"; # filler blank line
+			for (my $idx = 0; $idx<=100; $idx++) {
+				if ($cd_db[$idx][1] =~ m/$search_input/gi) {
+					print "\tTrack:\t$cd_db[$idx][0]\n";
+					print "\tArtist:\t$cd_db[$idx][1]\n";
+					print "\tAlbum:\t$cd_db[$idx][2]\n";
+					print "\tYear:\t$cd_db[$idx][3]\n";
+				}
+			}
+		} elsif ($menu_input == 3) { #album
 			print "Album to search for: ";
 			chomp($search_input = <>);
 			print "\n"; # filler blank line
 			for (my $idx = 0; $idx<=100; $idx++) {
 				if ($cd_db[$idx][2] =~ m/$search_input/gi) {
+					print "\tTrack:\t$cd_db[$idx][0]\n";
+					print "\tArtist:\t$cd_db[$idx][1]\n";
+					print "\tAlbum:\t$cd_db[$idx][2]\n";
+					print "\tYear:\t$cd_db[$idx][3]\n";
+				}
+			}
+		} elsif ($menu_input == 4) { #year
+			print "Year to search for: ";
+			chomp($search_input = <>);
+			print "\n"; # filler blank line
+			for (my $idx = 0; $idx<=100; $idx++) {
+				if ($cd_db[$idx][3] =~ m/$search_input/gi) {
 					print "\tTrack:\t$cd_db[$idx][0]\n";
 					print "\tArtist:\t$cd_db[$idx][1]\n";
 					print "\tAlbum:\t$cd_db[$idx][2]\n";
