167a12b65094ef32a7889ffa119c7b7c1b97b031
Caleb Cushing ( xenoterracide ) <xenoterracide@gmail.com>
2008-10-22 10:50:01 -0400

    update template to be blank, and contain my info

--- java/template.java
+++ java/template.java
@@ -1,6 +1,5 @@
 /**************************************************************
- * *** Date: 10/19/2008
- * *** Name: Jeff Morris
- * *** Purpose: This code accepts two integer values from the
- * *** keyboard, redisplays the values and their sum.
+ * *** Date:
+ * *** Name: Caleb Cushing
+ * *** Purpose:
  * **************************************************************/
