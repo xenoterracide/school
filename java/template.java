28b0f930dbea679b5c4d6920da4b0ca159fc06cd
Caleb Cushing ( xenoterracide ) <xenoterracide@gmail.com>
2008-10-22 10:48:35 -0400

    add the original template specified

---
+++ java/template.java
@@ -0,0 +1,6 @@
+/**************************************************************
+ * *** Date: 10/19/2008
+ * *** Name: Jeff Morris
+ * *** Purpose: This code accepts two integer values from the
+ * *** keyboard, redisplays the values and their sum.
+ * **************************************************************/
