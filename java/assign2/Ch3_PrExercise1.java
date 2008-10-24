e46458151096dc6f16fb872341c7a3bbe6a84890
Caleb Cushing ( xenoterracide ) <xenoterracide@gmail.com>
2008-10-24 10:35:52 -0400

    add Ch3 PE#1a

---
+++ java/assign2/Ch3_PrExercise1.java
@@ -0,0 +1,16 @@
+/**************************************************************
+ * *** Date: 10/22/2008
+ * *** Name: Caleb Cushing
+ * *** Purpose:
+ * **************************************************************/
+
+import java.util.Scanner;
+import java.io.FileReader;
+import java.io.PrintWriter;
+
+public class Ch3_PrExercise1
+{
+	public static void main(String[] args)
+	{
+	}
+}
