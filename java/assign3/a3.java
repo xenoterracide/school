651479d6fca2b3578d9394d80b2b425c50e6642b
Caleb Cushing ( xenoterracide ) <xenoterracide@gmail.com>
2008-10-29 01:36:59 -0400

    update to include skeleton framework

--- java/assign3/a3.java
+++ java/assign3/a3.java
@@ -3,3 +3,9 @@
  * *** Name: Caleb Cushing
  * *** Purpose: allow users to enter 1-4 or a-e
  * **************************************************************/
+public class a3
+{
+	public static void main(String[] args)
+	{
+	}
+}
