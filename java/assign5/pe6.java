cc285098877f1840fa869feeceb00f1930ecea9d
Caleb Cushing ( xenoterracide ) <xenoterracide@gmail.com>
2008-11-08 16:31:38 -0500

    add the guts of the apache WordUtils swapCase method

--- java/assign5/pe6.java
+++ java/assign5/pe6.java
@@ -3,15 +3,62 @@
  * *** Name: Caleb Cushing
  * *** Purpose: convert lower to upper and vice versa
  * **************************************************************/
-import javax.swing.JOptionPane;
+/*
+ * This file contains modified works -
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import javax.swing.*;
+import java.awt.*
+import java.awt.event.*;
 
-public class pe6
+public class pe6 extends JFrame
 {
 	public static void main(String[] args)
 	{
-		String StrIn;
+		String str;
 		String StrOut;
 
-		StrIn = JOptionPane.showInputDialog("Enter String to be converted");
+        int strLen;
+        if (str == null || (strLen = str.length()) == 0) {
+            return str;
+        }
+        StringBuffer buffer = new StringBuffer(strLen);
+
+        boolean whitespace = true;
+        char ch = 0;
+        char tmp = 0;
+
+        for (int i = 0; i < strLen; i++) {
+            ch = str.charAt(i);
+            if (Character.isUpperCase(ch)) {
+                tmp = Character.toLowerCase(ch);
+            } else if (Character.isTitleCase(ch)) {
+                tmp = Character.toLowerCase(ch);
+            } else if (Character.isLowerCase(ch)) {
+                if (whitespace) {
+                    tmp = Character.toTitleCase(ch);
+                } else {
+                    tmp = Character.toUpperCase(ch);
+                }
+            } else {
+                tmp = ch;
+            }
+            buffer.append(tmp);
+            whitespace = Character.isWhitespace(ch);
+        }
+        StrOut = buffer.toString();
 	}
 }
