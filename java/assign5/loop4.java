c4cccc62dbc511ebb9e4f4255722f70d3569d8cb
Caleb Cushing ( xenoterracide ) <xenoterracide@gmail.com>
2008-11-08 13:29:06 -0500

    add loop4

---
+++ java/assign5/loop4.java
@@ -0,0 +1,22 @@
+/**************************************************************
+ * *** Date: Sat Nov  8 13:21:12 EST 2008
+ * *** Name: Caleb Cushing
+ * *** Purpose: count down from 30 to zero, printing each number, except 27 
+ * until the program reaches 5
+ * **************************************************************/
+public class loop4
+{
+	public static void main(String[] args)
+	{
+		int i = 30;
+		do {
+			if (i != 27) {
+				System.out.println(i);
+				if (i == 5) {
+					break;
+				}
+			}
+			i--;
+		} while (i != 0);
+	}
+}
