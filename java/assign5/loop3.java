122786f0ad2590e9992b14e88c1f9f02314c1538
Caleb Cushing ( xenoterracide ) <xenoterracide@gmail.com>
2008-11-08 13:20:51 -0500

    add loop3

---
+++ java/assign5/loop3.java
@@ -0,0 +1,19 @@
+/**************************************************************
+ * *** Date: Sat Nov  8 12:43:42 EST 2008
+ * *** Name: Caleb Cushing
+ * *** Purpose:  count down every odd number from 40 to zero, printing each odd 
+ * number down to zero, except the number 3 and 33.  
+ * **************************************************************/
+public class loop3
+{
+	public static void main(String[] args)
+	{
+		for (int i = 40; i != 0; i--) {
+			// test to make sure numbers are odd and not 33 or 3
+			if ( (i % 2) == 0 || i == 33 || i == 3 ) {
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+}
