9854ad5785ddea7a52c6eaa92667893483f25190
Caleb Cushing ( xenoterracide ) <xenoterracide@gmail.com>
2008-11-08 13:17:02 -0500

    add loop2

---
+++ java/assign5/loop2.java
@@ -0,0 +1,21 @@
+/**************************************************************
+ * *** Date:
+ * *** Name: Caleb Cushing
+ * *** Purpose:
+ * **************************************************************/
+public class loop2
+{
+	public static void main(String[] args)
+	{
+		int i = 30;
+		while (i != 0) {
+			i--;
+			if (i != 27) {
+				System.out.println(i);
+				if (i == 5) {
+					break;
+				}
+			}
+		}
+	}
+}
