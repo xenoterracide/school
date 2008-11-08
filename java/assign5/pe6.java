0ca45fe1bc052b13c95902e87268cee7e4d2f38f
Caleb Cushing ( xenoterracide ) <xenoterracide@gmail.com>
2008-11-08 14:42:09 -0500

    prompt for input

---
+++ java/assign5/pe6.java
@@ -0,0 +1,17 @@
+/**************************************************************
+ * *** Date: Sat Nov  8 14:33:02 EST 2008
+ * *** Name: Caleb Cushing
+ * *** Purpose: convert lower to upper and vice versa
+ * **************************************************************/
+import javax.swing.JOptionPane;
+
+public class pe6
+{
+	public static void main(String[] args)
+	{
+		String StrIn;
+		String StrOut;
+
+		StrIn = JOptionPane.showInputDialog("Enter String to be converted");
+	}
+}
